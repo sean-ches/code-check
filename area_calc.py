4e535a1a02077df42bab69b1b7cd5999dc29aba0
Sean Cheskis <sean@cheskis.com>
2020-03-06 18:54:03 +0200

    update test

--- area_calc.py
+++ area_calc.py
@@ -1,36 +1,44 @@
 shape = input('Enter shape to calculate, must be "SQUARE", "TRIANGLE", "RECTANGLE", "TRAPEZOID":')
 
-class Calc():
 
-    base = int(input('Enter base:'))
-    height = int(input('Enter height:'))
-    if shape == 'TRAPEZOID' : base2 = int(input('Enter second base size:'))
+class Calc():
+    if shape == 'TRAPEZOID': base2 = int(input('Enter second base size:'))
 
     def __init__(self, height, base):
         self.height = height
         self.base = base
+        base = int(input('Enter base:'))
+        height = int(input('Enter height:'))
+
 
 class Square(Calc):
 
-    def cal():
-        return print('Calculated square area:', Calc.base * Calc.height)
+    def __init__(self, height, base):
+        super().__init__(height, base)
+
+    def getArea(self):
+        return self.base * self.height
+
 
 class Triangle(Calc):
 
-    def cal():
+    def cal(self):
         return print('Calculated triangle area:', Calc.base * Calc.height / 2)
+
 
 class Rectangle(Calc):
 
-    def cal():
+    def cal(self):
         return print('Calculated rectangle area:', Calc.base * Calc.height)
+
 
 class Trapezoid(Calc):
 
-    def cal():
+    def cal(self):
         return print('Calculated trapezoid area:', (Calc.base + Calc.base2) * Calc.height / 2)
 
-if shape == 'SQUARE' : Square.cal()
-if shape == 'TRIANGLE' : Triangle.cal()
-if shape == 'RECTANGLE' : Rectangle.cal()
-if shape == 'TRAPEZOID' : Trapezoid.cal()
+
+if shape == 'SQUARE': Square.getArea()
+if shape == 'TRIANGLE': Triangle.cal()
+if shape == 'RECTANGLE': Rectangle.cal()
+if shape == 'TRAPEZOID': Trapezoid.cal()
