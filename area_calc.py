6142a52b07dbee59115a6eb8b6ca198f7c3047a6
Sean Cheskis <sean@cheskis.com>
2020-03-07 04:54:58 +0200

    Area calculator done

--- area_calc.py
+++ area_calc.py
@@ -1,8 +1,4 @@
-shape = input('Enter shape to calculate, must be "SQUARE", "TRIANGLE", "RECTANGLE", "TRAPEZOID":')
-
-
 class Calc():
-#    if shape == 'TRAPEZOID': base2 = int(input('Enter second base size:'))
 
     def __init__(self):
         self.base = int(input('Enter base:'))
@@ -28,24 +24,42 @@ class Rectangle(Calc):
 
 class Trapezoid(Calc):
 
-     def __init__(self, height, base, base2):
-         super().__init__(height, base)
+     def __init__(self):
+         super().__init__()
          self.base2 = int(input("Enter second base for Trapezoid:"))
 
      def getArea(self):
          return (self.base + self.base2) * self.height / 2
 
 
-if shape == 'SQUARE':
-    Square = Square()
-    print ("Square Area is" , Square.getArea())
-if shape == 'TRIANGLE':
-    Triangle = Triangle()
-    print ("Triangle Area is" , Triangle.getArea())
-if shape == 'RECTANGLE':
-    Rectangle = Rectangle()
-    print ("Rectangle Area is", Rectangle.getArea())
-if shape == 'TRAPEZOID':
-    Trapezoid = Trapezoid()
-    print ("Trapezoid Area is", Trapezoid.getArea())
+results = []
+while True:
+    shape = input('Enter shape to calculate, must be "SQUARE", "TRIANGLE", "RECTANGLE" or "TRAPEZOID":')
 
+    if shape == 'SQUARE':
+        square = Square()
+        print ("Square Area is" , square.getArea(), 'Type "DONE" when finished.')
+        results.append(square.getArea())
+        continue
+    elif shape == 'TRIANGLE':
+        triangle = Triangle()
+        print ("Triangle Area is" , triangle.getArea(), 'Type "DONE" when finished.')
+        results.append(triangle.getArea())
+        continue
+    elif shape == 'RECTANGLE':
+        rectangle = Rectangle()
+        print ("Rectangle Area is", rectangle.getArea(), 'Type "DONE" when finished.')
+        results.append(rectangle.getArea())
+        continue
+    elif shape == 'TRAPEZOID':
+        trapezoid = Trapezoid()
+        print ("Trapezoid Area is", trapezoid.getArea(), 'Type "DONE" when finished.')
+        results.append(trapezoid.getArea())
+        continue
+    elif shape == 'DONE': break
+
+    else:
+        print('Unrecognized shape, try again.')
+        continue
+
+print('Process done! \nMaximum element is:', max(results),'\nMinimum element is:',min(results))
