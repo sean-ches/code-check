378899d303333912735c28c269f9757d8e525081
Sean Cheskis <sean@cheskis.com>
2020-03-14 13:12:10 +0200

    refactoring after review

--- sphere_calc.py
+++ sphere_calc.py
@@ -1,16 +1,18 @@
+import math
+
 class Base:
     def __init__(self):
         self.radius = int(input("Enter radius:"))
-        self.PI = 3.14
+        self.PI = math.pi
+
+    def getRadius(self):
+        return self.radius
 
 
 class Sphere(Base):
 
     def getVolume(self):
         return (3 / 4) * self.PI * self.radius ** 3
-
-    def getRadius(self):
-        return self.radius
 
 
 class Cylinder(Base):
@@ -22,9 +24,6 @@ class Cylinder(Base):
     def getVolume(self):
         return self.PI * self.radius ** 2 * self.height
 
-    def getRadius(self):
-        return self.radius
-
 
 class SphereCap(Base):
 
@@ -35,28 +34,24 @@ class SphereCap(Base):
     def getVolume(self):
         return (self.PI * self.height ** 2) / 3 * (3 * self.radius - self.height)
 
-    def getRadius(self):
-        return self.radius
-
 
 results = []
 while True:
     shape = input('Select shape to calculate - "SPH", "CYL" or "CAP", type "DONE" when done:')
-
     if shape == 'SPH':
-        sphere_area = Sphere()
-        print("Volume of sphere:", sphere_area.getVolume())
-        vol_result = {'Radius': sphere_area.getRadius(), 'Volume': sphere_area.getVolume(), 'Shape': 'Sphere'}
+        sphere_volume = Sphere()
+        print("Volume of sphere:", sphere_volume.getVolume())
+        vol_result = {'Radius': sphere_volume.getRadius(), 'Volume': sphere_volume.getVolume(), 'Shape': 'Sphere'}
 
     elif shape == 'CYL':
-        cyl_area = Cylinder()
-        print("Volume of cylinder:", cyl_area.getVolume())
-        vol_result = {'Radius': cyl_area.getRadius(), 'Volume': cyl_area.getVolume(), 'Shape': 'Cylinder'}
+        cyl_volume = Cylinder()
+        print("Volume of cylinder:", cyl_volume.getVolume())
+        vol_result = {'Radius': cyl_volume.getRadius(), 'Volume': cyl_volume.getVolume(), 'Shape': 'Cylinder'}
 
     elif shape == 'CAP':
-        cap_area = SphereCap()
-        print("Volume of sphere cap:", cap_area.getVolume())
-        vol_result = {'Radius': cap_area.getRadius(), 'Volume': cap_area.getVolume(), 'Shape': 'Cap'}
+        cap_volume = SphereCap()
+        print("Volume of sphere cap:", cap_volume.getVolume())
+        vol_result = {'Radius': cap_volume.getRadius(), 'Volume': cap_volume.getVolume(), 'Shape': 'Cap'}
 
     elif shape == 'DONE':
         break
@@ -64,6 +59,7 @@ while True:
     else:
         print("Unrecognized shape, try again!")
         continue
+    print(vol_result)
     results.append(vol_result)
 
 radius = [result['Radius'] for result in results]
