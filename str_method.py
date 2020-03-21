7a5c66705b03adec08398b4ebe987199c555f800
Sean Cheskis <sean@cheskis.com>
2020-03-21 04:44:38 +0200

    ver 0.1

--- str_method.py
+++ str_method.py
@@ -1,34 +1,63 @@
 import re
 
-# String to look for
-string = 'Is this a program? Yes, it is one, it will count how many: colons, commas, dots, and question marks are in the string.'
+# Handle sentences file
+file = open('random sentences.txt')
 
-split_str = re.findall(r"[\w']+", string)
-print(split_str)
-# Count length of string
-print('Length of string:', len(string), '\nAmount of words:', len(split_str))
+# First word list
+first_word = []
 
-# Count specific things in string
-print('---------------------------------')
-print('Amount of dots:', string.count('.'))
-print('Amount of commas:', string.count(','))
-print('Amount of spaces:', string.count(' '))
-print('Amount of colons:', string.count(':'))
-print('Amount of question marks:', string.count('?'))
-print('---------------------------------\n')
+for string in file:
 
-# Count "a" "an" and "the" in sentence
-print('Amount of "a" in sentence: ', split_str.count('a'))
-print('Amount of "an" in sentence: ', split_str.count('an'))
-print('Amount of "the" in sentence: ', split_str.count('the'))
+    # Splits
+    split_str = re.findall(r"[\w']+", string)
+    print(split_str)
+    # Count length of string
+    print('Length of string:', len(string), '\nAmount of words:', len(split_str))
 
-# Find biggest word
-length_of_word = 0
-for word in split_str:
-    if len(word) > length_of_word:
-        length_of_word = len(word)
-        biggest_word = word
-print('Biggest word in the sentence is:', biggest_word)
+    # Count specific things in string
+    print('---------------------------------')
+    print('Amount of dots:', string.count('.'))
+    print('Amount of commas:', string.count(','))
+    print('Amount of spaces:', string.count(' '))
+    print('Amount of colons:', string.count(':'))
+    print('Amount of question marks:', string.count('?'))
+    print('---------------------------------\n')
+
+    # Count "a" "an" and "the" in sentence
+    print('Amount of "a" in sentence:', split_str.count('a'))
+    print('Amount of "an" in sentence:', split_str.count('an'))
+    print('Amount of "the" in sentence:', split_str.count('the'))
+
+    # Find biggest word
+    length_of_word = 0
+    for word in split_str:
+        if len(word) > length_of_word:
+            length_of_word = len(word)
+            biggest_word = word
+    print('Biggest word in the sentence is:', biggest_word)
+
+
+
+    print('\nFirst word in sentence is:', split_str[0])
+    first_word.append(split_str[0])
+
+
+print('\n\nAdditional data:\n')
+
+# First words: Biggest
+length_of_first_word = 0
+for word in first_word:
+    if len(word) > length_of_first_word:
+        length_of_first_word = len(word)
+        biggest_first_word = word
+
+
+print('Biggest first word is:', biggest_first_word)
+
+# First words: no space string
+
+
+print(''.join(first_word))
 
 
 
